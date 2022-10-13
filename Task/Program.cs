43f54faa0aace977bfef1af9c9aaa1d566efba49
burmasovanton <looseangel1@gmail.com>
2022-10-13 23:03:48 +0500

    initial

---
+++ Task/Program.cs
@@ -0,0 +1,48 @@
+﻿
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+string[] GetMatrix( int n)
+{
+
+
+string[] matrix = new string[n];
+
+
+ for(int i = 0; i < n; i++)
+ {
+  Console.WriteLine("Введите элемент массива");
+  
+    matrix[i] = Console.ReadLine();
+
+ }
+
+ for(int i = 0; i < n; i++)
+ {
+  Console.Write($"{matrix[i]}, ");
+
+ }
+
+  return matrix;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int n = ReadNumber("Введите количество элементов в массиве");
+string[] matr = GetMatrix(n);
+
+
